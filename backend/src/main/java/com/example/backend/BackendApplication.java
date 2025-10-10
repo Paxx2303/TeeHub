cd1bbe8b90b93d1bda355aae1c616780aaf5922a
Pax2303 <namnguyen230304@gmail.com>
2025-10-10 09:19:09 +0700

    update backend

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package com.example.TeeHub;
+package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TeeHubApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TeeHubApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
